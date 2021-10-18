e4f0f9a43c57cd2b467593524cb65165e105fd37
unknown <shauryaakhileshsharma@gmail.com>
2021-10-18 12:23:37 +0530

    C137

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './screens/Home';
+import DetailsScreen from './screens/Details';
+
+export default function App() {
+  return (
+    <AppContainer />
+  );
+}
+
+const AppStackNavigator = createStackNavigator(
+  {
+    Home: {screen: HomeScreen, navigationOptions: {headerShown: false}},
+    Details: {screen: DetailsScreen}
+  },
+  {
+    initialRouteName: "Home"
+  }
+)
+
+const AppContainer = createAppContainer(AppStackNavigator)
